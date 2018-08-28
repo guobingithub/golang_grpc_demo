70fe1c738a64f9f9c2722af3af8a80ac84ff47f3
guobin <596194783@qq.com>
2018-08-28 10:20:52 +0800

    项目godep集成

--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
 #生成.go文件的脚本命令如下：
-1.cd gRPC
+1.cd golang_grpc_demo  (项目根目录)
 2.protoc --go_out=. ./serializeDemo/example/person.proto
     生成person.pb.go
 3.protoc --go_out=. ./proto/hello.proto
     生成hello.pb.go
+
+#godep集成命令如下：
+1.cd golang_grpc_demo  (项目根目录)
+2.godep save ./serializeDemo/...
+    生成Godeps和vendor文件夹(都在根目录), 里面有相应的依赖包信息
